e7433ff5ec3ec321a8d03e10ee9499998ef02572
Mohan Das <mohandast52@gmail.com>
2022-01-05 20:16:47 +0530

    Mohan/testnet (#7)
    * working on testnet implementation
    
    * working on testnet implementation
    
    * functions working for transactions
    
    * functions working for transactions
    
    * inputs added for demo
    
    * added env.local
    
    * updated comments

---
+++ components/MenuTest/styles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  .ant-input-group-wrapper {
+    margin-bottom: 16px;
+    max-width: 600px;
+    display: block;
+  }
+  .ant-btn {
+    margin-bottom: 16px;
+  }
+`;
+
+export const EmptyMessage = styled.div``;
