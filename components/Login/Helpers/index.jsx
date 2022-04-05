63f37e447e0b288c8054d53de7bde2aa8c076be8
mohandast52 <mohandast52@gmail.com>
2022-04-05 16:13:56 +0530

    move supportedNetworks to utils/constant file

--- components/Login/Helpers/index.jsx
+++ components/Login/Helpers/index.jsx
@@ -1,8 +1,8 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
-import { CHAIN_ID } from 'util/constants';
+import { SUPPORTED_NETWORKS, CHAIN_ID } from 'util/constants';
 
 const injected = new InjectedConnector({
-  supportedNetworks: [1, 3, 4, 5, 42],
+  supportedNetworks: SUPPORTED_NETWORKS,
   supportedChainIds: [CHAIN_ID],
 });
 
