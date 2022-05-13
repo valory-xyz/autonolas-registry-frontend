71ba0eba999c912175ee4d0ae7be8a7efe29a789
mohandast52 <mohandast52@gmail.com>
2022-05-13 19:36:44 +0530

    update contract address

--- common-util/AbiAndAddresses/serviceRegistry.jsx
+++ common-util/AbiAndAddresses/serviceRegistry.jsx
@@ -1,4 +1,4 @@
-export const SERVICE_REGISTRY_ADDRESS = '0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0';
+export const SERVICE_REGISTRY_ADDRESS = '0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82';
 
 export const SERVICE_REGISTRY = {
   _format: 'hh-sol-artifact-1',
