829df4af5046593b858e74e2fad0575355540647
mohandast52 <mohandast52@gmail.com>
2022-11-08 23:09:35 +0530

    my components to display as expected

--- components/ListComponents/index.jsx
+++ components/ListComponents/index.jsx
@@ -1,10 +1,12 @@
-import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Tabs } from 'antd/lib';
 import { useRouter } from 'next/router';
 import { URL, NAV_TYPES } from 'util/constants';
 import ListTable from 'common-util/List/ListTable';
 import { useExtraTabContent } from 'common-util/List/ListTable/helpers';
+import { getFirstAndLastIndex } from 'common-util/functions';
 import {
   getComponents,
   getComponentsByAccount,
@@ -22,6 +24,34 @@ const ListComponents = ({ account }) => {
   });
 
   const onViewClick = (id) => router.push(`${URL.COMPONENTS}/${id}`);
+
+  // My components
+  const [myComponentsList, setMyComponentsList] = useState([]);
+
+  // TODO: ask backend to create a separate function
+  const getMyComponentsApi = async () => {
+    const e = await getComponentsByAccount(account);
+    setMyComponentsList(e);
+  };
+
+  // fetch componentsByAccount behind
+  useEffect(() => {
+    if (account) {
+      (async () => {
+        await getMyComponentsApi();
+      })();
+    }
+  }, [account]);
+
+  const getMyComponents = async (myComponentsTotal, nextPage) => {
+    if (myComponentsList.length === 0) {
+      await getMyComponentsApi();
+    }
+
+    const { first, last } = getFirstAndLastIndex(myComponentsTotal, nextPage);
+    const myList = myComponentsList.slice(first - 1, last);
+    return new Promise((resolve) => resolve(myList));
+  };
 
   return (
     <>
@@ -45,7 +75,7 @@ const ListComponents = ({ account }) => {
         <TabPane tab="My Components" key="my_components">
           <ListTable
             type={NAV_TYPES.COMPONENT}
-            getList={getComponentsByAccount}
+            getList={getMyComponents}
             filterValue={searchValue}
             onViewClick={onViewClick}
             getTotal={() => getTotalForMyComponents(account)}
--- components/ListComponents/utils.jsx
+++ components/ListComponents/utils.jsx
@@ -1,9 +1,9 @@
 import { notification } from 'antd/lib';
-import { TOTAL_VIEW_COUNT } from 'util/constants';
 import {
   getMechMinterContract,
   getComponentContract,
 } from 'common-util/Contracts';
+import { getFirstAndLastIndex } from 'common-util/functions';
 
 // --------- HELPER METHODS ---------
 export const getComponentOwner = (id) => new Promise((resolve, reject) => {
@@ -52,42 +52,7 @@ export const getComponentDetails = (id) => new Promise((resolve, reject) => {
     });
 });
 
-export const getTotalForMyComponents = (account) => new Promise((resolve, reject) => {
-  const contract = getComponentContract();
-  contract.methods
-    .balanceOf(account)
-    .call()
-    .then((response) => {
-      resolve(response);
-    })
-    .catch((e) => {
-      reject(e);
-    });
-});
-
-export const getComponentsByAccount = (total, nextPage) => new Promise((resolve, reject) => {
-  const contract = getComponentContract();
-
-  try {
-    const allComponentsPromises = [];
-    const first = (nextPage - 1) * TOTAL_VIEW_COUNT + 1;
-    const last = Math.min(nextPage * TOTAL_VIEW_COUNT, total);
-    for (let i = first; i <= last; i += 1) {
-      const componentId = `${i}`;
-      const result = contract.methods.getUnit(componentId).call();
-      allComponentsPromises.push(result);
-    }
-
-    getComponentsHelper(allComponentsPromises, resolve);
-  } catch (e) {
-    console.error(e);
-    reject(e);
-  }
-});
-
-/**
- * Function to return all components
- */
+// totals
 export const getTotalForAllComponents = () => new Promise((resolve, reject) => {
   const contract = getComponentContract();
   contract.methods
@@ -101,24 +66,62 @@ export const getTotalForAllComponents = () => new Promise((resolve, reject) => {
     });
 });
 
+export const getTotalForMyComponents = (account) => new Promise((resolve, reject) => {
+  const contract = getComponentContract();
+  contract.methods
+    .balanceOf(account)
+    .call()
+    .then((response) => {
+      resolve(response);
+    })
+    .catch((e) => {
+      reject(e);
+    });
+});
+
+export const getComponentsByAccount = async (account) => {
+  const contract = getComponentContract();
+  const total = await getTotalForAllComponents();
+
+  return new Promise((resolve, reject) => {
+    try {
+      const allComponentsPromises = [];
+      for (let i = 1; i <= total; i += 1) {
+        const componentId = `${i}`;
+        const result = contract.methods.getUnit(componentId).call();
+        allComponentsPromises.push(result);
+      }
+
+      Promise.all(allComponentsPromises).then(async (componentsList) => {
+        const results = await Promise.all(
+          componentsList.map(async (info, i) => {
+            const owner = await getComponentOwner(`${i + 1}`);
+            return { ...info, owner };
+          }),
+        );
+
+        const finalResult = results.filter(
+          (e) => (e.owner || '').toLowerCase() === (account || '').toLowerCase(),
+        );
+        resolve(finalResult);
+      });
+    } catch (e) {
+      console.error(e);
+      reject(e);
+    }
+  });
+};
+
+/**
+ * Function to return all components
+ */
 export const getComponents = (total, nextPage) => new Promise((resolve, reject) => {
   const contract = getComponentContract();
 
   try {
     const allComponentsPromises = [];
-    /**
-       * @example
-       * TOTAL_VIEW_COUNT = 10
-       * nextPage = 5
-       * total = 45
-       * first = ((5 - 1) * 10) + 1
-       *      = (4 * 10) + 1
-       *      = 41
-       * last = min(5 * 10, 45)
-       *      = 45
-       */
-    const first = (nextPage - 1) * TOTAL_VIEW_COUNT + 1;
-    const last = Math.min(nextPage * TOTAL_VIEW_COUNT, total);
+
+    const { first, last } = getFirstAndLastIndex(total, nextPage);
     for (let i = first; i <= last; i += 1) {
       const componentId = `${i}`;
       const result = contract.methods.getUnit(componentId).call();
