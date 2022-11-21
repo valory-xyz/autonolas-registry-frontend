6abb4ea823eaaa58b26993593bf6b460bbba3017
mohandast52 <mohandast52@gmail.com>
2022-11-21 18:35:01 +0530

    working

--- common-util/Details/ServiceState/3rdStepFinishedRegistration/test.jsx
+++ common-util/Details/ServiceState/3rdStepFinishedRegistration/test.jsx
@@ -1,62 +1,32 @@
+/* eslint-disable consistent-return */
+import { get } from 'lodash';
+// import Web3 from 'web3';
 
-// import WalletConnect from '@walletconnect/browser';
-// import WalletConnectQRCodeModal from '@walletconnect/qrcode-modal';
+export const fetchTransactionDetails = () => {};
 
-// export const walletConnectInit = async () => {
-//   // bridge url
-//   const bridge = 'https://bridge.walletconnect.org';
+// const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
-//   // create new walletConnector
-//   const walletConnector = new WalletConnect({ bridge });
+// const web3 = new Web3(window.WEB3_PROVIDER || window.web3.currentProvider);
 
-//   window.walletConnector = walletConnector;
+export async function poll(hash) {
+  const url = `https://safe-transaction-goerli.safe.global/api/v1/multisig-transactions/${hash}`;
 
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(async () => {
+      console.log('Attempting to get transaction receipt...');
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        const isRight = get(json, 'isSuccessful');
 
-//   // check if already connected
-//   if (!walletConnector.connected) {
-//     // create new session
-//     await walletConnector.createSession();
-
-//     // get uri for QR Code modal
-//     const { uri } = walletConnector;
-
-//     // console log the uri for development
-//     console.log(uri); // tslint:disable-line
-
-//     // display QR Code modal
-//     WalletConnectQRCodeModal.open(uri, () => {
-//       console.log('QR Code Modal closed'); // tslint:disable-line
-//     });
-//   }
-
-//   walletConnector.on('connect', async (error, payload) => {
-//     console.log('walletConnector.on("connect")'); // tslint:disable-line
-
-//     if (error) {
-//       throw error;
-//     }
-
-//     console.log(payload);
-
-
-//     const customRequest = {
-//       id: 12374,
-//       jsonrpc: '2.0',
-//       method: 'gs_multi_send',
-//       params: [
-//         {
-//           to: '0x05c85Ab5B09Eb8A55020d72daf6091E04e264af9',
-//           value: '100000000000000000',
-//         },
-//         {
-//           to: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea',
-//           data: '0xa9059cbb00000000000000000000000005c85Ab5B09Eb8A55020d72daf6091E04e264af90000000000000000000000000000000000000000000000000de0b6b3a7640000',
-//         },
-//       ],
-//     };
-
-//     // send message
-//     const result = await walletConnector.sendCustomRequest(customRequest);
-//     console.log(result);
-//   });
-// };
+        if (isRight) {
+          clearInterval(interval);
+          return resolve(json);
+        }
+      } catch (error) {
+        clearInterval(interval);
+        return reject(error);
+      }
+    }, 3000);
+  });
+}
