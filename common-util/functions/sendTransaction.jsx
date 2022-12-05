0ecb2b99649d297a996ed0e2f0a6a821f6013f3c
mohandast52 <mohandast52@gmail.com>
2022-12-05 16:22:22 +0530

    adding poll

--- common-util/functions/sendTransaction.jsx
+++ common-util/functions/sendTransaction.jsx
@@ -1,5 +1,36 @@
 import { ethers } from 'ethers';
+import { get } from 'lodash';
 import { safeSendTransactionNotification } from './index';
+
+
+const getUrl = (hash, chainId) => {
+  if (chainId === 5) { return `${process.env.NEXT_PUBLIC_GNOSIS_SAFE_API_GOERLI}/${hash}`; }
+  return `${process.env.NEXT_PUBLIC_GNOSIS_SAFE_API_MAINNET}/${hash}`;
+};
+
+export async function pollTransactionDetails(hash, chainId) {
+  return new Promise((resolve, reject) => {
+    /* eslint-disable-next-line consistent-return */
+    const interval = setInterval(async () => {
+      window.console.log('Attempting to get transaction receipt...');
+
+      try {
+        const response = await fetch(getUrl(hash, chainId));
+        const json = await response.json();
+        const isSuccessful = get(json, 'isSuccessful');
+
+        if (isSuccessful) {
+          window.console.log('Transaction details: ', json);
+          clearInterval(interval);
+          return resolve(json);
+        }
+      } catch (error) {
+        clearInterval(interval);
+        return reject(error);
+      }
+    }, 3000);
+  });
+}
 
 /**
  * poll until the hash has been approved before deploy
@@ -7,6 +38,7 @@ import { safeSendTransactionNotification } from './index';
 export const sendTransaction = (
   sendFn,
   account,
+  extra,
   // {
   //   functionParams,
   //   sendParams,
@@ -15,6 +47,11 @@ export const sendTransaction = (
   //   eventFilters,
   // }
 ) => new Promise((resolve, reject) => {
+  const {
+    contract,
+    eventFilters,
+  } = extra;
+
   const provider = new ethers.providers.Web3Provider(
     window.MODAL_PROVIDER || window.web3.currentProvider,
     'any',
@@ -22,7 +59,7 @@ export const sendTransaction = (
 
   provider
     .getCode(account)
-    .then((code) => {
+    .then(async (code) => {
       const isGnosisSafe = code !== '0x';
 
       if (isGnosisSafe) {
@@ -33,7 +70,46 @@ export const sendTransaction = (
            * - return response
            */
         safeSendTransactionNotification();
+
+        /**
+         * 1. check if gnosis-safe?
+         *    if yes => use `transactionHash`, get the hash
+         *    then poll until we get the blockNumber
+         *    then return the resolve with Output?
+         * 2. if not-gnosis-safe use the custom
+         */
+        /* eslint-disable-next-line consistent-return */
+        const interval = setInterval(async () => {
+          window.console.log('Attempting to getPastEvents...');
+
+          try {
+            const filter = { owner: account, ...(eventFilters || {}) };
+            const startingBlock = await provider.getBlockNumber();
+
+            const pastEvents = await contract.getPastEvents('ApproveHash', {
+              filter,
+              fromBlock: startingBlock - 10,
+              toBlock: 'latest',
+            });
+            window.console.log('pastEvents:', pastEvents);
+
+            const hashApproved = pastEvents.length !== 0;
+            if (hashApproved) {
+              window.console.log('hashApproved');
+              clearInterval(interval);
+              return resolve();
+            }
+          } catch (error) {
+            clearInterval(interval);
+            return reject(error);
+          }
+        }, 5000);
       } else {
+        /**
+       * usual send function (right now supported by metamask)
+       */
+
+
         sendFn
           .then((receipt) => {
             resolve(receipt);
@@ -42,41 +118,6 @@ export const sendTransaction = (
             reject(e);
           });
       }
-
-      // /**
-      //  * usual send function (right now supported by metamask)
-      //  */
-
-      // /**
-      //    * 1. check if gnosis-safe?
-      //    *    if yes => use `transactionHash`, get the hash
-      //    *    then poll until we get the blockNumber
-      //    *    then return the resolve with Output?
-      //    * 2. if not-gnosis-safe use the custom
-      //    */
-      // // eslint-disable-next-line consistent-return
-      // const interval = setInterval(async () => {
-      //   window.console.log('Attempting to getPastEvents...');
-
-      //   try {
-      //     const pastEvents = await contract.getPastEvents('ApproveHash', {
-      //       filter: eventFilters,
-      //       fromBlock: startingBlock - 10,
-      //       toBlock: 'latest',
-      //     });
-      //     window.console.log('pastEvents:', pastEvents);
-
-      //     const hashApproved = pastEvents.length !== 0;
-      //     if (hashApproved) {
-      //       window.console.log('hashApproved');
-      //       clearInterval(interval);
-      //       return resolve();
-      //     }
-      //   } catch (error) {
-      //     clearInterval(interval);
-      //     return reject(error);
-      //   }
-      // }, 5000);
     })
     .catch(() => {
       console.error('Error on fetching code');
