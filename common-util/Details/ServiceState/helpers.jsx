91e92439987bcb95b5c6aaa46da8e25cd5360077
mohandast52 <mohandast52@gmail.com>
2023-02-27 15:49:42 +0530

    add lable

---
+++ common-util/Details/ServiceState/helpers.jsx
@@ -0,0 +1,11 @@
+export const SERVICE_STATE_HELPER_LABELS = {
+  'pre-registration': 'The service has just been registered.',
+  'active-registration':
+    'The service is waiting for agent operators to register their agent instances on the service.',
+  'finished-registration':
+    'All agent instance slots have been filled and waiting for the service owner to continue deploying the service.',
+  deployed:
+    'The service is in default operational state and the agent operators can turn on their agent instances at this point.',
+  terminated:
+    'The service has been terminated by the service owner and the service is waiting for the owner to unbond all registered agents.',
+};
