f6590b7997d78c4d1b154a98667fbd9040c9ce03
Mohan <mohandast52@gmail.com>
2023-02-27 16:05:36 +0530

    Update common-util/Details/ServiceState/helpers.jsx
    Co-authored-by: jmoreira-valory <96571377+jmoreira-valory@users.noreply.github.com>

--- common-util/Details/ServiceState/helpers.jsx
+++ common-util/Details/ServiceState/helpers.jsx
@@ -5,7 +5,7 @@ export const SERVICE_STATE_HELPER_LABELS = {
   'finished-registration':
     'All agent instance slots have been filled. Waiting for the service owner to continue deploying the service.',
   deployed:
-    'The service is in default operational state and the agent operators can turn on their agent instances at this point.',
+    'The service is in default operational state. Agent operators can turn on their agent instances at this point.',
   terminated:
     'The service has been terminated by the service owner and the service is waiting for the owner to unbond all registered agents.',
 };
