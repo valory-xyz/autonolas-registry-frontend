fb46918a4812cdf47b56837322874493a12daf66
mohandast52 <mohandast52@gmail.com>
2023-06-08 18:12:01 +0530

    export path map removed

--- next.config.js
+++ next.config.js
@@ -25,11 +25,6 @@ module.exports = {
   images: {
     unoptimized: true,
   },
-  async exportPathMap() {
-    return {
-      '/': { page: '/' },
-    };
-  },
   publicRuntimeConfig: {
     INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID,
     INFURA_PROJECT_SECRET: process.env.INFURA_PROJECT_ID,
