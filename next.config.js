48387de09de8cdf46784d7ea6eefd097ed0e2948
mohandast52 <mohandast52@gmail.com>
2023-06-26 13:12:15 +0530

    remove unwanted

--- next.config.js
+++ next.config.js
@@ -20,12 +20,6 @@ module.exports = {
       return config;
     },
   }),
-  publicRuntimeConfig: {
-    INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID,
-    INFURA_PROJECT_SECRET: process.env.INFURA_PROJECT_ID,
-    INFURA_TO_PUBLIC_ADDRESS: process.env.INFURA_TO_PUBLIC_ADDRESS,
-    TEST_NETWORK: process.env.TEST_NETWORK,
-  },
   compiler: {
     styledComponents: true,
   },
