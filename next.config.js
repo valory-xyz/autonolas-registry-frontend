3f952ffb6de2c0764210b483a7e51768b51c2da8
mohandast52 <mohandast52@gmail.com>
2023-08-17 15:06:14 +0530

    add analyzer

--- next.config.js
+++ next.config.js
@@ -1,26 +1,30 @@
 /* eslint-disable no-param-reassign */
 const withAntdLess = require('next-plugin-antd-less');
+const withBundleAnalyzer = require('@next/bundle-analyzer');
 const path = require('path');
 
-module.exports = {
-  ...withAntdLess({
-    reactStrictMode: true,
-    lessVarsFilePath: './styles/variables.less',
-    lessVarsFilePathAppendToEndOfContent: false,
-    cssLoaderOptions: {},
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-    },
-    webpack(config) {
-      config.resolve.alias.react = path.resolve('./node_modules/react');
-      config.resolve.alias['react-dom'] = path.resolve('./node_modules/react-dom');
-      config.resolve.alias.antd = path.resolve('./node_modules/antd');
-      config.resolve.alias['styled-components'] = path.resolve('./node_modules/styled-components');
+module.exports = withBundleAnalyzer(
+  {
+    enabled: process.env.ANALYZE === 'true',
+    ...withAntdLess({
+      reactStrictMode: true,
+      lessVarsFilePath: './styles/variables.less',
+      lessVarsFilePathAppendToEndOfContent: false,
+      cssLoaderOptions: {},
+      lessLoaderOptions: {
+        javascriptEnabled: true,
+      },
+      webpack(config) {
+        config.resolve.alias.react = path.resolve('./node_modules/react');
+        config.resolve.alias['react-dom'] = path.resolve('./node_modules/react-dom');
+        config.resolve.alias.antd = path.resolve('./node_modules/antd');
+        config.resolve.alias['styled-components'] = path.resolve('./node_modules/styled-components');
 
-      return config;
+        return config;
+      },
+    }),
+    compiler: {
+      styledComponents: true,
     },
-  }),
-  compiler: {
-    styledComponents: true,
   },
-};
+);
