e46a3f943848677564191e1313b00b8811fbc3e0
mohandast52 <mohandast52@gmail.com>
2023-08-17 16:50:45 +0530

    update nexjts config

--- next.config.js
+++ next.config.js
@@ -3,26 +3,28 @@ const withAntdLess = require('next-plugin-antd-less');
 // const withBundleAnalyzer = require('@next/bundle-analyzer');
 const path = require('path');
 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  reactStrictMode: true,
   ...withAntdLess({
-    reactStrictMode: true,
-    lessVarsFilePath: './styles/variables.less',
+    lessVarsFilePath: path.resolve('./styles/variables.less'),
     lessVarsFilePathAppendToEndOfContent: false,
     cssLoaderOptions: {},
     lessLoaderOptions: {
       javascriptEnabled: true,
     },
-    webpack(config) {
-      config.resolve.alias.react = path.resolve('./node_modules/react');
-      config.resolve.alias['react-dom'] = path.resolve('./node_modules/react-dom');
-      config.resolve.alias.antd = path.resolve('./node_modules/antd');
-      config.resolve.alias['styled-components'] = path.resolve('./node_modules/styled-components');
-
-      return config;
-    },
   }),
   compiler: {
     styledComponents: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias.react = path.resolve('./node_modules/react');
+    config.resolve.alias['react-dom'] = path.resolve('./node_modules/react-dom');
+    config.resolve.alias.antd = path.resolve('./node_modules/antd');
+    config.resolve.alias['styled-components'] = path.resolve('./node_modules/styled-components');
+    return config;
   },
 };
 
