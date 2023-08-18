cf3f90246a55156d2a1e3d96fe67ad61ef93f01c
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-08-18 16:01:48 +0100

    chore: adding a test to check chain configurations

--- tests/test-chains/TestChains.test.jsx
+++ tests/test-chains/TestChains.test.jsx
@@ -10,40 +10,64 @@ import {
   SERVICE_MANAGER_TOKEN_CONTRACT,
   OPERATOR_WHITELIST_CONTRACT,
 } from 'common-util/AbiAndAddresses';
+import { ADDRESSES } from 'common-util/Contracts';
 import fs from "fs";
+var assert = require('assert');
 
 describe('Test Chains', () => {
   it('Check contract addresses and ABIs', async () => {
-    const aaa = JSON.stringify(COMPONENT_REGISTRY_CONTRACT.abi);
-//     //console.log(aaa);
-//     const registriesRepo = 'https://github.com/valory-xyz/autonolas-registries/blob/main/';
-//     const response = await fetch(registriesRepo + 'docs/configuration.json');
-//     const data = await response.json();
-//     console.log(data);
+    // Try to do fetch. For now, the file is in local configuration.json
+    //const registriesRepo = 'https://github.com/valory-xyz/autonolas-registries/blob/main/';
+    //const response = await fetch(registriesRepo + 'docs/configuration.json');
+    //const data = await response.json();
 
-//   const [fileContent, setFileContent] = useState('');
-//
-//   useEffect(() => {
-//     async function fetchFile() {
-//       try {
-//         const registriesRepo = 'https://github.com/valory-xyz/autonolas-registries/blob/main/';
-//         const response = await fetch(registriesRepo + 'docs/configuration.json');
-//         if (response.ok) {
-//           const content = await response.json();
-//           console.log(content);
-//           setFileContent(content);
-//         } else {
-//           console.error('Failed to fetch file:', response.statusText);
-//         }
-//       } catch (error) {
-//         console.error('Error fetching file:', error);
-//       }
-//     }
-//
-//     fetchFile();
-//   }, []);
+    const localArtifacts = [
+      COMPONENT_REGISTRY_CONTRACT,
+      AGENT_REGISTRY_CONTRACT,
+      REGISTRIES_MANAGER_CONTRACT,
+      SERVICE_REGISTRY_CONTRACT,
+      SERVICE_REGISTRY_TOKEN_UTILITY_CONTRACT,
+      SERVICE_REGISTRY_L2,
+      SERVICE_MANAGER_CONTRACT,
+      SERVICE_MANAGER_TOKEN_CONTRACT,
+      OPERATOR_WHITELIST_CONTRACT
+    ];
 
+    const curDir = 'tests/test-chains/'
+    const configName = curDir + 'configuration.json';
+    const dataFromJSON = fs.readFileSync(configName, 'utf8');
+    const parsedConfig = JSON.parse(dataFromJSON);
 
+    // Loop over chains
+    const numChains = parsedConfig.length
+    for (let i = 0; i < numChains; i++) {
+      const contracts = parsedConfig[i]["contracts"];
+      // Traverse all tup-to-date configuration contracts
+      for (let j = 0; j < contracts.length; j++) {
+        // Go over local artifacts
+        for (let k = 0; k < localArtifacts.length; k++) {
+          // Take the configuration and local contract names that match
+          if (contracts[j]["name"] === localArtifacts[k].contractName) {
+            // Get local and configuration ABIs, stringify them
+            const localABI = JSON.stringify(localArtifacts[k].abi);
+            // TODO: update with fetching the URL
+            // Get up-to-date remote contract artifact and its ABI
+            const remoteJSON = fs.readFileSync(curDir + contracts[j]["artifact"], 'utf8');
+            const remoteArtifact = JSON.parse(remoteJSON);
+            // Stringify the remote ABI and compare with the local one
+            const remoteABI = JSON.stringify(remoteArtifact["abi"]);
+            assert(localABI === remoteABI);
 
+            // Check the address
+            const lowLetter = localArtifacts[k].contractName.charAt(0).toLowerCase() + localArtifacts[k].contractName.slice(1);
+            // Need to stringify and then convert to JSON again to access the address field
+            const addressStruct = JSON.stringify(ADDRESSES[parsedConfig[i]["chainId"]]);
+            const addressStructJSON = JSON.parse(addressStruct);
+            const localAddress = addressStructJSON[lowLetter];
+            assert(localAddress === contracts[j]["address"]);
+          }
+        }
+      }
+    }
   });
 });
