345dc0a90702252c9a67f3c437c19898cf354435
Aleksandr Kuperman <95347597+kupermind@users.noreply.github.com>
2023-08-25 10:49:31 +0100

    test: checking against multisig addresses in the configuration (#208)
    * test: checking against multisig addresses in the configuration
    
    * eslint fix and move constant outside the test
    
    ---------
    
    Co-authored-by: mohandast52 <mohandast52@gmail.com>

--- tests/test-chains/TestChains.test.jsx
+++ tests/test-chains/TestChains.test.jsx
@@ -12,28 +12,32 @@ import {
   SERVICE_MANAGER_TOKEN_CONTRACT,
   OPERATOR_WHITELIST_CONTRACT,
 } from 'common-util/AbiAndAddresses';
-import { ADDRESSES } from 'common-util/Contracts';
+import {
+  ADDRESSES,
+  multisigAddresses,
+  multisigSameAddresses,
+} from 'common-util/Contracts';
 
 const fetch = require('node-fetch');
+
+const localArtifacts = [
+  COMPONENT_REGISTRY_CONTRACT,
+  AGENT_REGISTRY_CONTRACT,
+  REGISTRIES_MANAGER_CONTRACT,
+  SERVICE_REGISTRY_CONTRACT,
+  SERVICE_REGISTRY_TOKEN_UTILITY_CONTRACT,
+  SERVICE_REGISTRY_L2,
+  SERVICE_MANAGER_CONTRACT,
+  SERVICE_MANAGER_TOKEN_CONTRACT,
+  OPERATOR_WHITELIST_CONTRACT,
+];
+const registriesRepo = 'https://raw.githubusercontent.com/valory-xyz/autonolas-registries/main/';
 
 describe('test-chains/TestChains.jsx', () => {
   it(
     'check contract addresses and ABIs',
     async () => {
-      const localArtifacts = [
-        COMPONENT_REGISTRY_CONTRACT,
-        AGENT_REGISTRY_CONTRACT,
-        REGISTRIES_MANAGER_CONTRACT,
-        SERVICE_REGISTRY_CONTRACT,
-        SERVICE_REGISTRY_TOKEN_UTILITY_CONTRACT,
-        SERVICE_REGISTRY_L2,
-        SERVICE_MANAGER_CONTRACT,
-        SERVICE_MANAGER_TOKEN_CONTRACT,
-        OPERATOR_WHITELIST_CONTRACT,
-      ];
-
       // Registries repository
-      const registriesRepo = 'https://raw.githubusercontent.com/valory-xyz/autonolas-registries/main/';
       // Fetch the actual config
       let response = await fetch(`${registriesRepo}docs/configuration.json`);
       const parsedConfig = await response.json();
@@ -46,8 +50,18 @@ describe('test-chains/TestChains.jsx', () => {
         for (let j = 0; j < contracts.length; j += 1) {
           // Go over local artifacts
           for (let k = 0; k < localArtifacts.length; k += 1) {
-            // Take the configuration and local contract names that match
-            if (contracts[j].name === localArtifacts[k].contractName) {
+            const { chainId } = parsedConfig[i];
+
+            if (contracts[j].name === 'GnosisSafeMultisig') {
+              // Check for the GnosisSafeMultisig address
+              expect(contracts[j].address).toBe(multisigAddresses[chainId][0]);
+            } else if (contracts[j].name === 'GnosisSafeSameAddressMultisig') {
+              // Check for the GnosisSafeSameAddressMultisig address
+              expect(contracts[j].address).toBe(
+                multisigSameAddresses[chainId][0],
+              );
+            } else if (contracts[j].name === localArtifacts[k].contractName) {
+              // Take the configuration and local contract names that match
               // Get local and configuration ABIs, stringify them
               const localABI = JSON.stringify(localArtifacts[k].abi);
 
@@ -63,9 +77,7 @@ describe('test-chains/TestChains.jsx', () => {
               const lowLetter = localArtifacts[k].contractName.charAt(0).toLowerCase()
                 + localArtifacts[k].contractName.slice(1);
               // Need to stringify and then convert to JSON again to access the address field
-              const addressStruct = JSON.stringify(
-                ADDRESSES[parsedConfig[i].chainId],
-              );
+              const addressStruct = JSON.stringify(ADDRESSES[chainId]);
               const addressStructJSON = JSON.parse(addressStruct);
               const localAddress = addressStructJSON[lowLetter];
               expect(localAddress).toBe(contracts[j].address);
