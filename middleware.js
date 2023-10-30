52c3fa765a6e0b0ac71b865e0b3a2a306782c28b
mohandast52 <mohandast52@gmail.com>
2023-10-30 20:24:58 +0530

    add page

--- middleware.js
+++ middleware.js
@@ -10,9 +10,21 @@ const prohibitedCountriesCode = Object.values(prohibitedCountries);
  */
 export default function validateCountryMiddleware(request) {
   const country = request.geo?.country;
+  const isProhibited = prohibitedCountriesCode.includes(country);
 
-  if (prohibitedCountriesCode.includes(country)) {
-    return NextResponse.error('Blocked for legal reasons', { status: 451 });
+  // if already on the not-legal page, don't redirect
+  if (request.nextUrl.pathname === '/not-legal') {
+    if (isProhibited) {
+      return NextResponse.next();
+    }
+
+    // if not prohibited & trying to access not-legal page, redirect to home
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  // if country is prohibited, redirect to not-legal page
+  if (isProhibited) {
+    return NextResponse.redirect(new URL('/not-legal', request.url));
   }
 
   return NextResponse.next();
