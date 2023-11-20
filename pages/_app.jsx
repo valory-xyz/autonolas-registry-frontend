843df5abfca3354a925fcf8dcaf75d61d71e06ff
Mohan <mohandast52@gmail.com>
2023-11-20 21:43:02 +0530

    fix (#224)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,49 +1,46 @@
-// import Head from 'next/head';
+import Head from 'next/head';
 import { createWrapper } from 'next-redux-wrapper';
-// import { ConfigProvider } from 'antd';
+import { ConfigProvider } from 'antd';
 import PropTypes from 'prop-types';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 
 /** wagmi config */
-// import { WagmiConfig as WagmiConfigProvider } from 'wagmi';
-// import { wagmiConfig } from 'common-util/Login/config';
+import { WagmiConfig as WagmiConfigProvider } from 'wagmi';
+import { wagmiConfig } from 'common-util/Login/config';
 
 /** antd theme config */
-// import Layout from 'components/Layout';
-// import GlobalStyle from 'components/GlobalStyles';
-// import { THEME_CONFIG } from '@autonolas/frontend-library';
+import Layout from 'components/Layout';
+import GlobalStyle from 'components/GlobalStyles';
+import { THEME_CONFIG } from '@autonolas/frontend-library';
 import initStore from '../store';
 
-// eslint-disable-next-line max-len
-// const DESC ='View and manage components, agents and services via the Autonolas on-chain registry.';
+const DESC = 'View and manage components, agents and services via the Autonolas on-chain registry.';
 
-const MyApp = () => 'Site in maintenance mode.';
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  const isNotLegal = router.pathname === '/not-legal';
 
-// const MyApp = ({ Component, pageProps }) => {
-//   const router = useRouter();
-//   const isNotLegal = router.pathname === '/not-legal';
-
-//   return (
-//     <>
-//       <GlobalStyle />
-//       <Head>
-//         <title>Autonolas Registry</title>
-//         <meta name="description" content={DESC} />
-//       </Head>
-//       <ConfigProvider theme={THEME_CONFIG}>
-//         {isNotLegal ? (
-//           <Component {...pageProps} />
-//         ) : (
-//           <WagmiConfigProvider config={wagmiConfig}>
-//             <Layout>
-//               <Component {...pageProps} />
-//             </Layout>
-//           </WagmiConfigProvider>
-//         )}
-//       </ConfigProvider>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <GlobalStyle />
+      <Head>
+        <title>Autonolas Registry</title>
+        <meta name="description" content={DESC} />
+      </Head>
+      <ConfigProvider theme={THEME_CONFIG}>
+        {isNotLegal ? (
+          <Component {...pageProps} />
+        ) : (
+          <WagmiConfigProvider config={wagmiConfig}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </WagmiConfigProvider>
+        )}
+      </ConfigProvider>
+    </>
+  );
+};
 
 MyApp.getInitialProps = async ({ Component, ctx }) => {
   const pageProps = Component.getInitialProps
