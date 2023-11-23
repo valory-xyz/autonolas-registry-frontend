37ba3ddb3aa176ef69accb6caa24d307fa7010b8
mohandast52 <mohandast52@gmail.com>
2023-11-24 05:24:10 +0530

    style src added

--- next.config.js
+++ next.config.js
@@ -45,7 +45,7 @@ module.exports = {
             isDev,
             contentSecurityPolicy: {
               'default-src': "'none'",
-              'script-src': "'self' wss://relay.walletconnect.org",
+              'script-src': "'self'",
               'connect-src': [
                 "'self'",
                 'wss://relay.walletconnect.org',
@@ -61,7 +61,14 @@ module.exports = {
                 'https://safe-transaction-goerli.safe.global/api/',
                 'https://safe-transaction-gnosis-chain.safe.global/api/',
                 'https://safe-transaction-polygon.safe.global/api/',
+                'wss://relay.walletconnect.com/',
+                'https://vercel.live', // TODO: check if we need to load in prod? (BTW, required for preview mode)
               ],
+              'img-src': [
+                "'self'",
+                'https://explorer-api.walletconnect.com/w3m/',
+              ],
+              'style-src': ["'self'", "'unsafe-inline'"],
             },
           }),
           {
@@ -73,12 +80,3 @@ module.exports = {
     ];
   },
 };
-
-/**
- *
- *
- * "'self'  https://rpc.walletconnect.com/ https://explorer-api.walletconnect.com https://eth-mainnet.g.alchemy.com/v2/ ttps://eth-goerli.g.alchemy.com/v2/ https://gno.getblock.io/ https://polygon-mainnet.g.alchemy.com/v2 https://polygon-mumbai-bor.publicnode.com https://rpc.chiado.gnosis.gateway.fm https://safe-transaction-mainnet.safe.global/api/v1/ https://safe-transaction-goerli.safe.global/api/ https://safe-transaction-gnosis-chain.safe.global/api/ https://safe-transaction-polygon.safe.global/api/",
- *
- *
- *
- */
