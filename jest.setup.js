5aa5950ac6abb97d690e140dfa95c8de720a44b6
mohandast52 <mohandast52@gmail.com>
2023-11-27 12:22:03 +0530

    init

--- jest.setup.js
+++ jest.setup.js
@@ -28,3 +28,55 @@ jest.mock('next/router', () => ({
   __esModule: true,
   useRouter: jest.fn(),
 }));
+
+const mockEthereumClient = {
+  disconnect: jest.fn(),
+  getAccount: jest.fn(),
+  watchAccount: jest.fn(),
+  fetchBalance: jest.fn(),
+  getNetwork: jest.fn(),
+  watchNetwork: jest.fn(),
+  switchNetwork: jest.fn(),
+  fetchEnsName: jest.fn(),
+  fetchEnsAvatar: jest.fn(),
+  connect: jest.fn(),
+};
+
+jest.mock('@web3modal/ethereum', () => ({
+  EthereumClient: mockEthereumClient,
+  w3mConnectors: jest.fn(),
+  w3mProvider: jest.fn(),
+
+}));
+
+jest.mock('wagmi', () => ({
+  configureChains: jest.fn(),
+  createConfig: jest.fn(),
+}));
+
+jest.mock('wagmi/chains', () => ({
+  mainnet: { chainId: 1, chainName: 'Ethereum Mainnet' },
+  goerli: { chainId: 5, chainName: 'Goerli Testnet' },
+  gnosis: { chainId: 100, chainName: 'Gnosis Safe' },
+  gnosisChiado: { chainId: 101, chainName: 'Gnosis Safe Chiado' },
+  polygon: { chainId: 137, chainName: 'Polygon Mainnet' },
+  polygonMumbai: { chainId: 80001, chainName: 'Polygon Mumbai Testnet' },
+}));
+
+jest.mock('wagmi/connectors/safe', () => ({
+  SafeConnector: jest.fn(),
+}));
+
+jest.mock('wagmi/providers/jsonRpc', () => ({
+  jsonRpcProvider: jest.fn(),
+}));
+
+jest.mock('publicClient', () => ({
+  getPublicClient: jest.fn(),
+}));
+
+// const mockConfigureChains = jest.fn().mockReturnValue({
+//   publicClient: mockEthereumClient,
+//   webSocketPublicClient: mockEthereumClient,
+//   chains: [],
+// });
