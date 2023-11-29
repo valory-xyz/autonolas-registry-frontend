121316ce58877658a7a7c21453d11548c11ef477
Mohan <mohandast52@gmail.com>
2023-11-29 16:00:41 +0530

    feat: CSP header  (#225)
    * do not allow inline or eval
    
    * allow only from same origin
    
    * blocking everything to tedst
    
    * allow everything
    
    * allow default src & script src
    
    * use nextSafe package for headers
    
    * connect-src added
    
    * style src added
    
    * update gitleaks
    
    * update config
    
    * minor update
    
    * add registry URL in connectSrc
    
    * support for clipboard
    
    * updates
    
    * remove unsafe-inline
    
    * add unsafe css script

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const nextSafe = require('next-safe');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
 module.exports = {
   reactStrictMode: true,
   compiler: {
@@ -30,23 +34,61 @@ module.exports = {
       permanent: true,
     },
   ],
+  /**
+   * Headers for all routes
+   * @see https://nextjs.org/docs/api-reference/next.config.js/headers
+   */
   async headers() {
+    const connectSrc = [
+      "'self'",
+      'https://*.autonolas.tech/',
+      'https://verify.walletconnect.org/',
+      'wss://relay.walletconnect.org/',
+      'https://rpc.walletconnect.com/',
+      'https://explorer-api.walletconnect.com/',
+      'https://eth-mainnet.g.alchemy.com/v2/',
+      'https://eth-goerli.g.alchemy.com/v2/',
+      'https://gno.getblock.io/',
+      'https://polygon-mainnet.g.alchemy.com/v2/',
+      'https://polygon-mumbai-bor.publicnode.com/',
+      'https://rpc.chiado.gnosis.gateway.fm/',
+      'https://safe-transaction-mainnet.safe.global/api/v1/',
+      'https://safe-transaction-goerli.safe.global/api/',
+      'https://safe-transaction-gnosis-chain.safe.global/api/',
+      'https://safe-transaction-polygon.safe.global/api/',
+      'wss://relay.walletconnect.com/',
+      'https://vercel.live/',
+    ];
+
+    if (isDev) {
+      connectSrc.push('http://localhost');
+      connectSrc.push('ws://localhost');
+    }
+
     return [
       {
         source: '/:path*',
         headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors 'none';",
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
+          ...nextSafe({
+            isDev,
+            /**
+             * Content Security Policy
+             * @see https://content-security-policy.com/
+             */
+            contentSecurityPolicy: {
+              'default-src': "'none'",
+              'script-src': "'self'",
+              'connect-src': connectSrc,
+              'img-src': [
+                "'self'",
+                'https://*.autonolas.tech/',
+                'https://explorer-api.walletconnect.com/w3m/',
+                'data:',
+              ],
+              'style-src': ["'self'", "'unsafe-inline'"],
+            },
+            permissionsPolicyDirectiveSupport: ['standard'],
+          }),
           {
             key: 'Strict-Transport-Security',
             value: 'max-age=31536000; includeSubDomains',
