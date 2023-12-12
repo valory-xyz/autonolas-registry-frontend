17f2c98427e44ed55b0cde7412a5b5ea79a80633
mohandast52 <mohandast52@gmail.com>
2023-12-13 02:15:19 +0530

    remove sha256

--- next.config.js
+++ next.config.js
@@ -43,11 +43,6 @@ module.exports = {
       'https://verify.walletconnect.org',
       'https://verify.walletconnect.com',
     ];
-    const scriptSha256 = [
-      "'sha256-YS3QNE5QXiUUIM5MeZS0LnchkXKr//rinpZZO+rodCY='",
-      "'sha256-avpYSMhuJi9WJxyEjI6GYMe1nMx5fWui+MNt0GpU354='",
-      "'sha256-7WNRe20drB93fDeE6wXp+mRpZz4QnQJ0MS4oT5S5bi4='",
-    ];
     const connectSrc = [
       "'self'",
       ...walletconnectSrc,
@@ -88,7 +83,7 @@ module.exports = {
              */
             contentSecurityPolicy: {
               'default-src': "'none'",
-              'script-src': ["'self'", ...scriptSha256],
+              'script-src': ["'self'", "'unsafe-inline'"],
               'connect-src': connectSrc,
               'img-src': [
                 "'self'",
