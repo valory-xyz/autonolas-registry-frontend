eb9507a9eaca88d1f556b62ce53fa724ed196daa
mohandast52 <mohandast52@gmail.com>
2024-01-12 01:06:38 +0530

    add documentation to useSvmInfo & useSvmData in ViewButton component

--- components/ListServices/ViewButton.jsx
+++ components/ListServices/ViewButton.jsx
@@ -1,15 +1,8 @@
+import { useCallback } from 'react';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import {
-  Program, AnchorProvider,
-  // BorshAccountsCoder,
-  BorshCoder,
-  //  BorshInstructionCoder,
-} from '@project-serum/anchor';
-// import { u32, u8, struct } from '@solana/buffer-layout';
-// import { publicKey, u64, bool } from '@solana/buffer-layout-utils';
+import { Program, AnchorProvider, BorshCoder } from '@project-serum/anchor';
 import idl from 'common-util/AbiAndAddresses/ServiceRegistrySolana.json';
 import {
-  // Connection, Keypair,
   PublicKey,
   TransactionMessage,
   VersionedTransaction,
@@ -22,45 +15,30 @@ import { Button } from 'antd';
 
 const programId = new PublicKey(SVM_SERVICE_REGISTRY_PROGRAM_PUBLIC_KEY);
 
-// export const MintLayout = struct([
-//   u8('mintAuthorityOption'),
-//   publicKey('mintAuthority'),
-//   u64('supply'),
-//   u8('decimals'),
-//   bool('isInitialized'),
-//   u32('freezeAuthorityOption'),
-//   publicKey('freezeAuthority'),
-// ]);
-
-// deseralize the program data using borsh
-const deseralizeProgramData = (data) => {
-  /**
-   * contains the struct of the service
-   * @example
-   * { name: "serviceOwner", type: "publicKey" },
-   * { name: "securityDeposit", type: "u64" },
-   */
-  // const serviceType = idl.types.find((t) => t.name === 'Service').type;
-
-  // create a struct for the service using the serviceType
-  // const Service = struct(serviceType);
-
-  // create a borsh coder for the service
-  const serviceCoder = new BorshCoder(idl);
-  const dataaaa = serviceCoder.types.decode('Service', data);
-
-  console.log({ dataaaa });
-
-  // const serviceType = idl.types.decode('ServiceType', decodedData.serviceType);
-  // console.log({ serviceType });
-
-  // const borshedData = idl.types.decode('ServiceRegistryData', data);
-  // console.log({ decodedData });
+/**
+ * deseralize the program data
+ * @xample
+ * serializedValue = Uint8Array(32) [0, 0, ...]
+ * @returns {object} deseralized program data
+ * example: { name: "serviceOwner", type: "publicKey" },
+ *
+ */
+const deseralizeProgramData = (serializedValue, decodeTypeName) => {
+  const borshCoder = new BorshCoder(idl);
+  const decodedResult = borshCoder.types.decode(
+    decodeTypeName,
+    serializedValue,
+  );
+  return decodedResult;
 };
 
-export default function ViewButton() {
-  const { publicKey, wallet } = useWallet();
+/**
+ * hook to get svm info
+ * @returns {object} publicKey, connection, program
+ */
+const useSvmInfo = () => {
   const { connection } = useConnection();
+  const { publicKey, wallet } = useWallet();
 
   const anchorProvider = new AnchorProvider(connection, wallet, {
     commitment: 'processed',
@@ -68,72 +46,69 @@ export default function ViewButton() {
 
   const program = new Program(idl, programId, anchorProvider);
 
-  const onClick = async () => {
-    console.log({
-      publicKey,
-      program,
-    });
+  return { publicKey, connection, program };
+};
 
-    if (!publicKey || !program) return;
+const useSvmData = () => {
+  const { publicKey, connection, program } = useSvmInfo();
 
-    // Build the instruction
-    const instruction = await program.methods
-      // .totalSupply()
-      .getService(1)
-      .accounts({ dataAccount: SVM_STORAGE_ACCOUNT_PUBLIC_KEY })
-      .instruction();
+  const getData = useCallback(
+    async (fn, fnArgs, decodeTypeName = 'Service') => {
+      if (!fn) {
+        throw new Error('function name is required');
+      }
 
-    const latestBlock = await connection.getLatestBlockhash();
+      if (!publicKey || !program) return null;
 
-    console.log({ instruction, latestBlock, program });
+      const latestBlock = await connection.getLatestBlockhash();
 
-    // Build a versioned transaction with the instruction
-    const txMessage = new TransactionMessage({
-      payerKey: publicKey,
-      recentBlockhash: latestBlock.blockhash,
-      instructions: [instruction],
-    }).compileToV0Message();
+      const instruction = await program.methods[fn](...(fnArgs || []))
+        .accounts({ dataAccount: SVM_STORAGE_ACCOUNT_PUBLIC_KEY })
+        .instruction();
 
-    const tx = new VersionedTransaction(txMessage);
+      const txMessage = new TransactionMessage({
+        payerKey: publicKey,
+        recentBlockhash: latestBlock.blockhash,
+        instructions: [instruction],
+      }).compileToV0Message();
 
-    // Simulate the transaction.
-    const transactionSimulation = await connection.simulateTransaction(tx);
-    console.log(transactionSimulation);
+      const tx = new VersionedTransaction(txMessage);
 
-    // If value is "0", then returnData returns null, so can't use this for bool
-    if (transactionSimulation.value.returnData?.data) {
-      console.log(
-        'actual returnedData',
-        transactionSimulation.value.returnData.data[0],
-      );
-    }
+      const transactionSimulation = await connection.simulateTransaction(tx);
 
-    // Log all the transaction logs.
-    const transactionLogs = transactionSimulation.value.logs;
-    console.log({ transactionLogs });
+      const transactionLogs = transactionSimulation.value.logs;
 
-    // As workaround to avoid null return data when value is "0"
-    // Extract the program return data directly from the logs.
-    // Find log entry that starts with "Program return:"
-    const returnPrefix = `Program return: ${program.programId} `;
-    console.log({ returnPrefix });
+      const returnPrefix = `Program return: ${program.programId} `;
+      const returnLogEntry = transactionLogs?.find((log) => log.startsWith(returnPrefix));
 
-    const returnLogEntry = transactionLogs?.find((log) => log.startsWith(returnPrefix));
-    console.log({ returnLogEntry });
+      if (returnLogEntry) {
+        const encodedReturnData = returnLogEntry.slice(returnPrefix.length);
+        const decodedBuffer = Buffer.from(encodedReturnData, 'base64');
 
-    if (returnLogEntry) {
-      // Slice out the prefix to get the base64 return data
-      const encodedReturnData = returnLogEntry.slice(returnPrefix.length);
-      console.log({ encodedReturnData });
+        const finalResult = deseralizeProgramData(
+          decodedBuffer,
+          decodeTypeName,
+        );
 
-      // Convert the Base64 return data
-      const decodedBuffer = Buffer.from(encodedReturnData, 'base64');
-      console.log('decodedBuffer ===>>> ', decodedBuffer);
-      deseralizeProgramData(decodedBuffer);
-      // console.log('decodedBuffer.toString ===>>> ', decodedBuffer);
-      console.log('first decodedBuffer - ', decodedBuffer[0]);
-    }
-  };
+        window.console.log({ finalResult }); // TODO: remove
 
-  return <Button onClick={onClick}>Load Service Total</Button>;
+        return finalResult;
+      }
+
+      return null;
+    },
+    [publicKey, program, connection],
+  );
+
+  return { getData };
+};
+
+export default function ViewButton() {
+  const { getData } = useSvmData();
+
+  const handleClick = useCallback(() => {
+    getData('getService', [1], 'Service');
+  }, [getData]);
+
+  return <Button onClick={handleClick}>Load Service Total</Button>;
 }
